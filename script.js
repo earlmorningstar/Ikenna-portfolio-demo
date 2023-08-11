44fda1eed3d48e637001c9f58710bf1c4ad6f1dd
Onyeabor Joel <earlmorningstar@gmail.com>
2023-08-11 12:42:10 +0100

    First Commit.

---
+++ script.js
@@ -0,0 +1,15 @@
+const navbar = document.querySelector('.navbar');
+const hero = document.querySelector('.hero-section');
+let lastScrollY = window.scrollY;
+
+window.addEventListener('scroll', () => {
+  const currentScrollY = window.scrollY;
+  
+  if (currentScrollY > lastScrollY) {
+    navbar.style.backgroundColor = '#014040cf'; 
+  } else {
+    const heroColor = getComputedStyle(hero).backgroundColor;
+    navbar.style.backgroundColor = heroColor;
+  }
+  lastScrollY = currentScrollY;
+});
